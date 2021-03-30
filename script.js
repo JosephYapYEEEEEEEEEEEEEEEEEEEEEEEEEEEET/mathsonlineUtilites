83045a61c80e26e8577b48421b2c8c27106b8fef
JosephYapYEEEEEEEEEEEEEEEEEEEEEEEEEEEET <75049883+JosephYapYEEEEEEEEEEEEEEEEEEEEEEEEEEEET@users.noreply.github.com>
2021-03-31 10:43:44 +1100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementsByClassName("dropup hidden")[0].classList = "dropup";
+function keyboard() {
+document.getElementById("ui__btn--keyboard").style = "display: inline;";
+}
+var a = window.setInterval(keyboard, 10);
---
+++ script2.js
@@ -0,0 +1,6 @@
+document.getElementsByTagName("div")[28].style = "padding-bottom:20px; display:inline";
+document.getElementsByTagName("h2")[1].style = "color: black;";
+function black(){
+document.getElementsByClassName("fc-today")[1].style.color = "black";
+}
+var a = window.setInterval(black, 10);
