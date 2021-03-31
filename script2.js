5fe177850adff0434005e2e0d109961ad0e2bfd9
JosephYapYEEEEEEEEEEEEEEEEEEEEEEEEEEEET <75049883+JosephYapYEEEEEEEEEEEEEEEEEEEEEEEEEEEET@users.noreply.github.com>
2021-03-31 14:58:23 +1100

    Update script2.js

--- script2.js
+++ script2.js
@@ -1,4 +1,7 @@
-document.getElementsByTagName("div")[28].style = "padding-bottom:20px; display:inline";
+
+var e = document.getElementsByClassName("container")[2]
+
+e.getElementsByTagName("div")[0].style = "padding-bottom:20px; display:inline";
 document.getElementsByTagName("h2")[1].style = "color: black;";
 function black(){
 document.getElementsByClassName("fc-today")[1].style.color = "black";
