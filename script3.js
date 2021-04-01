700654d5efdc5bc82c117a95506857dd2d5a8b25
JosephYapYEEEEEEEEEEEEEEEEEEEEEEEEEEEET <75049883+JosephYapYEEEEEEEEEEEEEEEEEEEEEEEEEEEET@users.noreply.github.com>
2021-04-01 11:42:20 +1100

    Update script3.js

--- script3.js
+++ script3.js
@@ -1,3 +1,1 @@
-document.getElementById("question-set-progress-bar").id = "";
-document.getElementById("question-set-grade").innerHTML = "\<strong\>\<span style=\"color: chocolate\"\>-1%\</span\>\</strong\>"
-document.getElementById("question-set-grade").id = "";
+
