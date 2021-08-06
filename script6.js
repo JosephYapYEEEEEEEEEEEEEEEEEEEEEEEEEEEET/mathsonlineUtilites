f3e482a8f7ca7d32ba7f3c9db12eb1119e693e9d
JosephYapYEEEEEEEEEEEEEEEEEEEEEEEEEEEET <75049883+JosephYapYEEEEEEEEEEEEEEEEEEEEEEEEEEEET@users.noreply.github.com>
2021-08-06 10:32:16 +1000

    Update script6.js

--- script6.js
+++ script6.js
@@ -1,11 +1,15 @@
 function runscript() {
-if(localStorage.checkedSettings != 1) {
-d = document.createElement("div");
-d.innerHTML = `1`;
-d.style = "position:absolute; top: 1px; right: 1px;";
-d.className = "badge round-3";
-d.id = "maths-settings-check"
-document.getElementsByTagName("li")[5].append(d);
-}
+  if(!!localStorage.checkedShowVideo) {
+    localStorage.checkedSettings = localStorage.checkedShowVideo;
+    localStorage.remove(checkedShowVideo);
+  }
+  if(localStorage.checkedSettings != 1) {
+    d = document.createElement("div");
+    d.innerHTML = `1`;
+    d.style = "position:absolute; top: 1px; right: 1px;";
+    d.className = "badge round-3";
+    d.id = "maths-settings-check"
+    document.getElementsByTagName("li")[5].append(d);
+  }
 }
 document.addEventListener('DOMContentLoaded', runscript);
